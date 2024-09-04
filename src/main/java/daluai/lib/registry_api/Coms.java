f587f6fed5955116c0c43ae0a3c8d335ff7acc5a
Diogo Aluai <diogoaluai@gmail.com>
2024-09-04 22:39:40 +0100

    Add Api key env and refactor

--- src/main/java/daluai/lib/registry_api/Coms.java
+++ src/main/java/daluai/lib/registry_api/Coms.java
@@ -2,20 +2,13 @@ package daluai.lib.registry_api;
 
 public class Coms {
 
-    public static final String REGISTRY_FLY_NAME_SYS_ENV = "COOL_REGISTRY_FLY_NAME";
-    public static String REGISTRY_FLY_NAME;
-    static {
-        String registryFlyNameSysEnvValue = System.getenv(REGISTRY_FLY_NAME_SYS_ENV);
-        if (registryFlyNameSysEnvValue == null || registryFlyNameSysEnvValue.isBlank()) {
-            throw new IllegalStateException("Value required for system env:" + REGISTRY_FLY_NAME_SYS_ENV);
-        }
-        REGISTRY_FLY_NAME = registryFlyNameSysEnvValue;
-    }
+    public static final String REGISTRY_FLY_NAME = getEnvOrFail("COOL_REGISTRY_FLY_NAME");
     public static final String REGISTRY_CLOUD_URL = "https://" + REGISTRY_FLY_NAME + ".fly.dev";
     public static final String REGISTRY_LOCAL_URL = "http://localhost:8080";
 
-
     public static final String SERVICE_NAME = "REGISTRY";
+
+    public static final String API_KEY = getEnvOrDefault("API_KEY_REGISTRY", "somekey");
 
     /**
      * Beware that changing these endpoints may affect non-java scripts that rely on them.
@@ -25,5 +18,21 @@ public class Coms {
     public static final String ENDPOINT_REGISTER = "/register";
     public static final String ENDPOINT_DEREGISTER = "/deregister";
     public static final String ENDPOINT_RESET = "/reset";
+
+    private static String getEnvOrFail(String env) {
+        String envValue = System.getenv(env);
+        if (envValue == null || envValue.isBlank()) {
+            throw new IllegalStateException("Value required for system env:" + env);
+        }
+        return envValue;
+    }
+
+    private static String getEnvOrDefault(String env, String defaultValue) {
+        String envValue = System.getenv(env);
+        if (envValue == null || envValue.isBlank()) {
+            return defaultValue;
+        }
+        return envValue;
+    }
 
 }
