6d4895352d5fb56d93988d94a899247d8a012ee0
Diogo Aluai <diogoaluai@gmail.com>
2024-09-05 22:02:37 +0100

    Add property management
    - Create PropertyManager.
    - Add config.properties files.
    - Move env variables to properties file.

--- src/main/java/daluai/lib/registry_api/Coms.java
+++ src/main/java/daluai/lib/registry_api/Coms.java
@@ -1,14 +1,16 @@
 package daluai.lib.registry_api;
 
+import daluai.lib.network_utils.property.PropertyManager;
+
+import static daluai.lib.network_utils.property.PropertyKeys.FLY_APP_NAME;
+
 public class Coms {
 
-    public static final String REGISTRY_FLY_NAME = getEnvOrFail("COOL_REGISTRY_FLY_NAME");
+    public static final String REGISTRY_FLY_NAME = PropertyManager.DEFAULT_INSTANCE.getProperty(FLY_APP_NAME);
     public static final String REGISTRY_CLOUD_URL = "https://" + REGISTRY_FLY_NAME + ".fly.dev";
     public static final String REGISTRY_LOCAL_URL = "http://localhost:8080";
 
     public static final String SERVICE_NAME = "REGISTRY";
-
-    public static final String API_KEY = getEnvOrDefault("API_KEY_REGISTRY", "somekey");
 
     /**
      * Beware that changing these endpoints may affect non-java scripts that rely on them.
@@ -18,21 +20,5 @@ public class Coms {
     public static final String ENDPOINT_REGISTER = "/register";
     public static final String ENDPOINT_DEREGISTER = "/deregister";
     public static final String ENDPOINT_RESET = "/reset";
-
-    private static String getEnvOrFail(String env) {
-        String envValue = System.getenv(env);
-        if (envValue == null || envValue.isBlank()) {
-            throw new IllegalStateException("Value required for system env:" + env);
-        }
-        return envValue;
-    }
-
-    private static String getEnvOrDefault(String env, String defaultValue) {
-        String envValue = System.getenv(env);
-        if (envValue == null || envValue.isBlank()) {
-            return defaultValue;
-        }
-        return envValue;
-    }
 
 }
