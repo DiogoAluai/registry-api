3deef37e27b7f80665895b4bc6708aa489bb0291
daluai <diogoaluai@gmail.com>
2026-05-19 00:04:10 +0100

    Move to java 25. Remove GitHub workflows

--- src/main/java/daluai/lib/registry_api/Service.java
+++ src/main/java/daluai/lib/registry_api/Service.java
@@ -1,7 +1,6 @@
 package daluai.lib.registry_api;
 
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public record Service(
@@ -12,12 +11,6 @@ public record Service(
         ServiceType type,
         String protocol
 ) implements Serializable {
-
-    @Serial
-    private static final long serialVersionUID = 1;
-
-    static public final String RELAYED_SERVICE_SUFFIX = "-relay";
-    static public final String PRIVATE_SERVICE_SUFFIX = "-private";
 
     /**
      * Constructor that assumes http/https protocol based on service type
@@ -50,13 +43,9 @@ public record Service(
      * CLOUD and OPEN services must have public ip.
      */
     private boolean isIpStateInvalid() {
-        return (type.equals(ServiceType.CLOUD) && publicIp.isBlank())
-                || (type.equals(ServiceType.PRIVATE) && privateIp.isBlank())
-                || (type.equals(ServiceType.OPEN) && (publicIp.isBlank()));
-    }
-
-    private boolean isRelayed() {
-        return name.endsWith(RELAYED_SERVICE_SUFFIX);
+        return (type.equals(ServiceType.CLOUD) && publicIp != null && publicIp.isBlank())
+                || (type.equals(ServiceType.PRIVATE) && privateIp != null && privateIp.isBlank())
+                || (type.equals(ServiceType.OPEN) && publicIp != null && publicIp.isBlank());
     }
 
     public String getPrivateUrl() {
@@ -71,12 +60,7 @@ public record Service(
      * Get service url, depending on service type
      */
     public String getUrl() {
-        String url = protocol + "://" + getIp() + ":" + port;
-        if (isRelayed()) {
-            // ~ if relayed, uri should have '/save/{service}' prefix
-            return url.concat("/save/" + name);
-        }
-        return url;
+        return protocol + "://" + getIp() + ":" + port;
     }
 
     /**
